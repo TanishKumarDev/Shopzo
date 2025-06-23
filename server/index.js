0d5b600cf36af2c034d063988d949ff5fcfddcf4
Tanish <tanish.kumar.contact@gmail.com>
2025-06-23 19:52:46 +0530

    Tested user login and OTP verification APIs

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './utils/database.js';
+import userRoutes from './routes/user.js';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+app.use(express.json()); // Middleware to parse JSON bodies
 
 connectDB();
 
+// Using routes
+app.use('/api', userRoutes);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
---
+++ server/models/otp.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const otpSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  otp: {
+    type: Number,
+    required: true
+  },
+  expire: {
+    type: Date,
+    default: () => new Date(Date.now() + 5 * 60 * 1000),
+    index: { expires: '5m' }
+  }
+});
+
+export const OTP = mongoose.model('OTP', otpSchema);
