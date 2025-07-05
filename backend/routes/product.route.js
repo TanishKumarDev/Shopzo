f59630a0f6bf811f96e7710b036f6ede5812d355
Tanish <tanish.kumar.contact@gmail.com>
2025-07-05 10:46:39 +0530

    Refactor product routes for improved organization and clarity

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,14 +1,27 @@
 import express from "express";
-import {  getFeaturedProducts, getAllProducts, createProduct, deleteProduct, getRecommendedProducts, toggleFeaturedProduct, getProductsByCategory} from "../controllers/product.controller.js";
+import {
+  getAllProducts,
+  getFeaturedProducts,
+  getRecommendedProducts,
+  getProductsByCategory,
+  createProduct,
+  deleteProduct,
+  toggleFeaturedProduct
+} from "../controllers/product.controller.js";
+
 import { protectRoute, adminRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, adminRoute, getAllProducts);
+// Public Routes
 router.get("/featured", getFeaturedProducts);
 router.get("/recommendations", getRecommendedProducts);
 router.get("/category/:category", getProductsByCategory);
-router.get("/", protectRoute, adminRoute, createProduct);
+
+// Protected Admin Routes
+router.get("/", protectRoute, adminRoute, getAllProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
 router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
-router.get("/:id", protectRoute, adminRoute, deleteProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
+
 export default router;
